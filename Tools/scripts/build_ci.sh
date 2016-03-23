7a55b5c16f42a15adf9955f0756774c0e44a1ee5
Francisco Ferreira <francisco-ferreira@mail.telepac.pt>
2016-03-23 22:15:38 +0000

    ci: disable building benchmarks in OSX

--- Tools/scripts/build_ci.sh
+++ Tools/scripts/build_ci.sh
@@ -85,7 +85,11 @@ for t in $CI_BUILD_TARGET; do
 
     if [[ -n ${waf_supported_boards[$t]} ]]; then
         echo "Starting waf build for board ${t}..."
-        $waf configure --board $t --enable-benchmarks --check-c-compiler="$c_compiler" --check-cxx-compiler="$cxx_compiler"
+        if [[ "$OS_NAME" == 'osx' ]]; then
+            $waf configure --board $t --check-c-compiler="$c_compiler" --check-cxx-compiler="$cxx_compiler"
+        else
+            $waf configure --board $t --enable-benchmarks --check-c-compiler="$c_compiler" --check-cxx-compiler="$cxx_compiler"
+        fi
         $waf clean
         $waf ${build_concurrency[$t]} all
         if [[ $t == linux ]]; then
