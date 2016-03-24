a3c01e340aeaccb1fd0ab854c3107b7c9d8ab632
Francisco Ferreira <francisco-ferreira@mail.telepac.pt>
2016-03-24 00:16:36 +0000

    waf: gbenchmark: try to set compiler to the same used by waf

--- Tools/ardupilotwaf/gbenchmark.py
+++ Tools/ardupilotwaf/gbenchmark.py
@@ -48,6 +48,8 @@ def libbenchmark(bld):
         cmake_vars=dict(
             CMAKE_BUILD_TYPE='Release',
             CMAKE_INSTALL_PREFIX=prefix_node.abspath(),
+            CMAKE_C_COMPILER=bld.env.get_flat('CC'),
+            CMAKE_CXX_COMPILER=bld.env.get_flat('CXX'),
         )
     )
 
