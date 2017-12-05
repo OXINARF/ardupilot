a7a4fbe3e0bab58741012918026d46f2af971b22
Francisco Ferreira <francisco-ferreira@mail.telepac.pt>
2017-12-05 18:49:21 +0000

    Copter: add new mission option
    This new option only allows mission upload or mission running to be happen independently, they can't happen at the same time
    If a mission upload is interrupted then a mission can't be run until it is re-uploaded

--- ArduCopter/control_auto.cpp
+++ ArduCopter/control_auto.cpp
@@ -20,7 +20,7 @@
 // auto_init - initialise auto controller
 bool Copter::auto_init(bool ignore_checks)
 {
-    if ((position_ok() && mission.num_commands() > 1) || ignore_checks) {
+    if ((position_ok() && mission.num_commands() > 1 && mission.can_run()) || ignore_checks) {
         auto_mode = Auto_Loiter;
 
         // reject switching to auto mode if landed with motors armed but first command is not a takeoff (reduce chance of flips)
